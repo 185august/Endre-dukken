b26e29afc512e2fdb6c05c81618893f8223acc1d
noname0000-blip <augusteide98@hotmail.com>
2025-03-14 09:29:48 +0100

    small stuff

--- js/imageView.js
+++ js/imageView.js
@@ -1,8 +1,10 @@
 function imageView() {
     html = `
+     
     ${createHtml('head')}
     ${createHtml('body')}
     ${createHtml('legs')}
+    
 `
     document.getElementById('app').innerHTML = html
 }
@@ -13,9 +15,10 @@ function updateView() {
 
 function createHtml(part) {
     return `
-    <button onclick="changeImg('${part}', -1)">◀</button>
-        <img src="img/${model.data[part][model.input.currentIndex[part]]}.png" alt="">
-    <button onclick="changeImg('${part}', 1)">▶</button>
-    <br>
-    `;
+   <div class="image-container">
+        <button onclick="changeImg('${part}', -1)">◀</button>
+            <img src="img/${model.data[part][model.input.currentIndex[part]]}.png" alt="">
+        <button onclick="changeImg('${part}', 1)">▶</button>
+   </div>
+        `;
 }
